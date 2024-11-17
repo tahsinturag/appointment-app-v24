a618e2973d3b1087012d855d4235aa30e7cf4b86
tahsinturag <tahsinturag10@gmail.com>
2024-11-17 16:48:47 +0600

    load appointments -ngOnInit - life cycles hook and use of local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,6 +1,8 @@
  import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
-@Component({
+import {OnInit} from "@angular/core";
+
+ @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
@@ -15,13 +17,23 @@ import { Appointment } from '../models/appointment';
 //   }
 // }
 
- export class AppointmentListComponent {
+ export class AppointmentListComponent implements OnInit {
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
-
   appointments: Appointment[] = []
   // newAppointmentTitle: any;
+  //  write below all our property
+   ngOnInit(): void {
+     // throw new Error('Method not implemented.');
+     // console.log("got loaded")
 
+     //   load appointments -ngOnInit - life cycles hook
+
+     let savedAppointments = localStorage.getItem('appointments');
+     this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+   //   if we have value then parsing the saved appointments and assigned them into appointments ;
+   //   else create empty array indicated by the column
+   }
 
 
 addAppointment() {
@@ -37,18 +49,25 @@ addAppointment() {
     this.newAppointmentTitle = "";//reset the input field
     this.newAppointmentDate = new Date();
     // alert(this.appointments.length)
+    //using local storage
+//  transform to JSON then save it to the local storage
+   localStorage.setItem("appointments", JSON.stringify(this.appointments))
+
     }
   }
 //  <!--delete item first have to create a new method-->
   deleteAppointment(index: number) {
   //    remove index using splice method
     this.appointments.splice(index, 1);
+  //   for update after delete
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+
+
+
+
   }
 }
 
-// //using local storage
-//    localStorage.setItem('appointments', JSON.stringify(this.appointments));
-//  };
 
 
 
